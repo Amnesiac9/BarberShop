7bb4a74d1009c5503c778bae7dcfa4cffeffe887
John <johnmoreau19@gmail.com>
2024-04-21 12:40:56 -0700

    Trying to get theme to work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css'
 import AppLayout from './AppLayout'
+import { ConfigProvider, theme } from 'antd';
+import { purple } from '@ant-design/colors';
+import type { ThemeConfig } from 'antd';
+import React from 'react';
+
+const { darkAlgorithm } = theme;
+
+// TODO: Switch algorithm based on user preference
+const globalThemeConfig: ThemeConfig = {
+    algorithm: darkAlgorithm,
+    token: {
+        colorPrimary: purple.primary,
+        borderRadius: 2,
+
+    },
+    components: {
+        Menu: { algorithm: true, horizontalItemBorderRadius: 8, itemBorderRadius: 8, },
+        Layout: { algorithm: true, borderRadius: 8 },
+    }
+}
 
 function App() {
+    const [themeConfig, setThemeConfig] = React.useState(globalThemeConfig)
 
     return (
-        <AppLayout />
+        <ConfigProvider theme={themeConfig}>
+            <AppLayout setThemeConfig={setThemeConfig} />
+        </ConfigProvider>
+
     )
 }
 
--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,17 +1,69 @@
 import React from 'react';
+import { Layout, Menu, theme, ColorPicker, ThemeConfig } from "antd";
+import { Color } from 'antd/es/color-picker';
 // import { Calendar } from "antd";
 // import { DatePicker } from 'antd';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
 
-function AppLayout() {
+const { Header, Content, Footer } = Layout
+
+// const { getDesignToken } = theme;
+// const globalToken = getDesignToken();
+
+const items = new Array(3).fill(null).map((_, index) => ({
+    key: String(index + 1),
+    label: `nav ${index + 1}`,
+}));
+
+function AppLayout(props: { setThemeConfig: React.Dispatch<React.SetStateAction<ThemeConfig>>; }) {
+
+    const {
+        token: { colorPrimary, colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    function updateThemeColor(color: Color) {
+        props.setThemeConfig((prevConfig: ThemeConfig) => {
+            return {
+                ...(prevConfig),
+                token: {
+                    colorPrimary: color.toHexString(),
+                }
+            }
+        })
+
+    }
 
     return (
         <>
-            <Header />
+            <Layout>
+                <Header style={{ position: 'sticky', top: 0, width: '100%', display: 'flex' }}>
+                    <div className='logo'>LogoHere</div>
+                    <Menu mode='horizontal' items={items} defaultSelectedKeys={['2']} style={{ flex: 1, minWidth: 0 }}></Menu>
+                    <div className='end'>
+                        <ColorPicker showText value={colorPrimary} onChangeComplete={(color) => updateThemeColor(color)} />
+                    </div>
+                </Header>
+                <Content style={{ padding: '0 48px', borderRadius: '25' }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: '100vh',
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        Content
+                        Content
 
-            <Footer />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Lucky Cuts Barbershop ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+
         </>
     )
 }
