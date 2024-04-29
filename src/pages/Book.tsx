892bb4cf3a36cfc1d8d90f0a91f36fb7337c5585
John <johnmoreau19@gmail.com>
2024-04-28 20:08:31 -0700

    Date picker and form value watch working.

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,5 +1,5 @@
 
-import { Button, DatePicker, Form, Input, Select } from 'antd';
+import { Button, DatePicker, Form, Input, Select, Typography } from 'antd';
 import type { FormProps } from 'antd';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
@@ -11,9 +11,9 @@ dayjs.extend(customParseFormat);
 
 type FieldType = {
     email?: string;
-    date?: Date;
+    date?: dayjs.Dayjs;
     service?: string;
-    time?: string;
+    // time?: string;
 };
 
 const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
@@ -32,18 +32,22 @@ function BookAppointment() {
     const [timeSlots, setTimeSlots] = React.useState<dayjs.Dayjs[]>([])
     const now = dayjs();
 
-    const dateValue = Form.useWatch('date', form);
+    const dateValue = Form.useWatch('date', form)
     const serviceValue = Form.useWatch('service', form);
+    const emailValue = Form.useWatch('email', form)
+
 
 
     useEffect(() => {
 
-        if (dateValue === undefined) {
-            console.log('dateValue is undefined')
-            return;
-        }
+
 
         const fetchAvailableTimeslots = async () => {
+            if (dateValue === undefined) {
+                console.log('dateValue is undefined')
+                return;
+            }
+            console.log('dateValue:', dateValue)
             try {
 
 
@@ -68,13 +72,12 @@ function BookAppointment() {
     return (
         <div>
             <h2>Book Appointment</h2>
-            {/* <Flex align='center' justify='center'> */}
             <div style={{ alignContent: 'center', alignItems: 'center', justifyContent: 'center', justifyItems: 'center', display: 'flex' }}>
                 <div style={{ width: '85%', marginLeft: 'auto', marginRight: 'auto' }}>
-                    <Form size='large' layout='horizontal' style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: 600 }}
+                    <Form form={form} size='large' layout='horizontal' style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: 600 }}
                         onFinish={onFinish} onFinishFailed={onFinishFailed}
                     >
-                        <Form.Item<FieldType> required label='Select a date' name='date' initialValue={now}
+                        <Form.Item<FieldType> required label='Select a date' name="date" initialValue={now}
                             rules={[{ required: true, message: 'Please select a date.' }]}
                         >
                             <DatePicker
@@ -82,16 +85,14 @@ function BookAppointment() {
                                 maxDate={now.add(30, 'day')}
                             />
                         </Form.Item>
-                        <Form.Item<FieldType> required label='Service' name='service'
+                        <Form.Item<FieldType> required label='Service' name="service"
                             rules={[{ required: true, message: 'Please select a service.' }]}
                         >
-                            <Select onChange={() => setServiceSelected(true)}>
+                            <Select>
                                 <Select.Option value='demo'>Demo</Select.Option>
                             </Select>
                         </Form.Item>
-                        <Form.Item<FieldType> label='Contact email' name='email'
-                            rules={[{ max: 100, message: 'Max length: 100 characters.' }]}
-                        >
+                        <Form.Item<FieldType> label='Contact email' name="email">
                             <Input />
                         </Form.Item>
 
@@ -107,11 +108,15 @@ function BookAppointment() {
                             <Button disabled={serviceSelected} style={{ marginTop: '25px' }} type='primary' htmlType="submit" size='large'>Submit</Button>
                         </Form.Item>
                     </Form>
-                    {/* </Flex> */}
-                    {/* </div> */}
                 </div>
             </div>
+            <Typography>
+                <pre>Email Value: {emailValue}</pre>
+                <pre>Service Value: {serviceValue || ''}</pre>
+                <pre>Date Value: {dateValue?.format()}</pre>
+            </Typography>
         </div>
+
     )
 }
 
