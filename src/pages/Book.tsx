c23911737d86d8842f6b02d571107c68dbeeb1e3
John <johnmoreau19@gmail.com>
2024-05-01 00:30:37 -0700

    Attempting to add button to switch date

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -35,12 +35,17 @@ const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
 };
 
 
+
+
+
+
 function BookAppointment() {
 
     const [form] = Form.useForm<FieldType>();
     // const [serviceSelected, setServiceSelected] = React.useState(false);
     const [timeSlots, setTimeSlots] = React.useState<TimeSlot[]>([])
-    const bookedTimes = React.useRef<Map<string, bool>>(new Map())
+    const bookedTimes = React.useRef<Map<string, boolean>>(new Map())
+    const [dateAdd, setDateAdd] = React.useState(0)
 
 
     const dateValue = Form.useWatch('date', form)
@@ -52,10 +57,15 @@ function BookAppointment() {
     // const firstAvailableDay = now.set('hour', 10)
 
 
+    const incrementDate = () => {
+        setDateAdd(dateAdd + 1)
+    }
+
+
     useEffect(() => {
         const fetchAvailableTimeslots = async () => {
             if (dateValue === undefined) {
-                console.log('dateValue is undefined')
+                // console.log('dateValue is undefined')
                 return;
             }
             // console.log('dateValue:', dateValue.format())
@@ -63,8 +73,8 @@ function BookAppointment() {
 
                 let currentDate = dateValue
 
-                if (dateValue.isSame(now, 'day')) {
-                    currentDate = currentDate.set('hour', now.hour())
+                if (dateValue.isSame(dayjs(), 'day')) {
+                    currentDate = currentDate.set('hour', dayjs().hour())
                 }
 
                 // Grab the dif between the selected time and the start time of the day. Subtract that from our available hours.
@@ -90,7 +100,7 @@ function BookAppointment() {
                 // Persist in memory in a map.
                 // These would normally be stored in a db.
                 // TODO: Grab user booked dates from internal storage?
-                const daysFromToday = dateValue.diff(firstAvailableDay, 'days')
+                const daysFromToday = dateValue.diff(dayjs(), 'days')
                 const timeslots: TimeSlot[] = [];
                 for (let i = 0; i < 8; ++i) {
 
@@ -99,7 +109,7 @@ function BookAppointment() {
                         booked: i < 8 - hoursLeft
                     }
                     if (bookedTimes.current.has(timeslot.time.format())) {
-                        timeslot.booked = bookedTimes.current.get(timeslot.time.format())
+                        timeslot.booked = bookedTimes.current.get(timeslot.time.format()) || false
                     } else {
                         const number = Math.random() * (100 - (daysFromToday * 2)) + 1
                         //console.log(number)
@@ -153,30 +163,14 @@ function BookAppointment() {
                                 rules={[{ required: true, message: 'Please select a time slot.' }]}
                             >
                                 <Radio.Group size='large' >
-                                    <Space direction='horizontal' wrap={true} size='middle' >
+                                    <Space key={'space'} direction='horizontal' wrap={true} size='middle' >
                                         {timeSlots.map((time, index) => (
-                                            <div>
-                                                {/* {time.booked && (
-  
-                                            )} */}
-                                                {/* <Badge.Ribbon placement='start' text='booked'>
-                                                    <Radio.Button disabled={time.booked} value={time.time}>{time.time.format('hh:mm A')}</Radio.Button>
-                                                </Badge.Ribbon> */}
-                                                <Badge key={'badge-' + index} showZero={time.booked} size='small' count={time.booked ? 'Booked' : 0} offset={[-30, 0]}>
-                                                    <Radio.Button key={'radio-' + index} disabled={time.booked} value={time.time}>{time.time.format('hh:mm A')}</Radio.Button>
-                                                </Badge>
-                                            </div>
-
+                                            <Badge key={'badge-' + index} showZero={time.booked} size='small' count={time.booked ? 'Booked' : 0} offset={[-30, 0]}>
+                                                <Radio.Button key={'radio-' + index} disabled={time.booked} value={time.time}>{time.time.format('hh:mm A')}</Radio.Button>
+                                            </Badge>
                                         ))}
-                                        {/* <Radio.Button value={1}>Option A</Radio.Button>
-                                        <Radio.Button value={2}>Option B</Radio.Button>
-                                        <Radio.Button value={3}>Option B</Radio.Button>
-                                        <Radio.Button value={4}>Option B</Radio.Button>
-                                        <Radio.Button value={5}>Option B</Radio.Button>
-                                        <Radio.Button value={6}>Option B</Radio.Button>
-                                        <Radio.Button value={7}>Option B</Radio.Button>
-                                        <Radio.Button value={8}>Option B</Radio.Button> */}
-                                        <Button type='text'>Next Day...</Button>
+                                        {/* //TODO: Fix this button. Maybe paginate instead... */}
+                                        <Button onClick={() => incrementDate()} type='text'>Next Day...</Button>
                                     </Space>
                                 </Radio.Group>
                             </Form.Item>
@@ -190,12 +184,12 @@ function BookAppointment() {
                     </Form>
                 </div>
             </div>
-            <Typography>
+            {/* <Typography>
                 <pre>Email Value: {emailValue}</pre>
                 <pre>Service Value: {serviceValue || ''}</pre>
                 <pre>Date Value: {dateValue?.format() ?? ''}</pre>
                 <pre>Available Times: {timeSlots.length}</pre>
-            </Typography>
+            </Typography> */}
         </div>
 
     )
